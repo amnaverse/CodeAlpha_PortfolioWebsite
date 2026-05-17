5691f7a1766bb7d1195561e15cd057fd3619bf84
Amna-14-ay <amna16528@gmail.com>
2026-05-17 00:25:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -123,3 +123,16 @@ particlesJS("particles-js",{
   retina_detect:true
 
 });
+function validateForm(){
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let message = document.getElementById("message").value;
+
+  if(name === "" || email === "" || message === ""){
+    alert("Please fill all fields!");
+    return false;
+  }
+
+  alert("Message sent successfully!");
+  return true;
+}
